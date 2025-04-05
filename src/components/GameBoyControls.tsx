5cb6d34ff14d56ed8022ff2f24ac024ee22229bb
beech80 <beechhouseprod@gmail.com>
2025-04-04 21:31:51 -0500

    Remove comment (chat) button from GameBoy controls

--- src/components/GameBoyControls.tsx
+++ src/components/GameBoyControls.tsx
@@ -411,16 +411,7 @@ const GameBoyControls: React.FC = () => {
           </svg>
         </button>
         
-        {/* Comment button (right) */}
-        <button 
-          className={`action-button b ${commentActive ? 'active' : ''}`}
-          onClick={() => handleActionButtonClick('comment')}
-          aria-label="Comment"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-          </svg>
-        </button>
+
         
         {/* Plus button (left) */}
         <button 
