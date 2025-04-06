c78f8f3d3280a66eb639d359371bf798d49a5a6b
beech80 <beechhouseprod@gmail.com>
2025-04-06 17:51:08 -0500

    Remove POST button and camera button from GameBoy controls

--- src/components/GameBoyControls.tsx
+++ src/components/GameBoyControls.tsx
@@ -370,12 +370,7 @@ const GameBoyControls: React.FC = () => {
             <line x1="3" y1="18" x2="21" y2="18"></line>
           </svg>
         </button>
-        <button className="menu-button menu-right camera-button" onClick={() => navigate('/create')} aria-label="Create post">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path>
-            <circle cx="12" cy="13" r="4"></circle>
-          </svg>
-        </button>
+
       </div>
 
       {/* Game Menu */}
@@ -451,18 +446,7 @@ const GameBoyControls: React.FC = () => {
           </svg>
         </button>
         
-        {/* Green POST button underneath */}
-        <button 
-          className="post-button"
-          onClick={() => handleActionButtonClick('post')}
-          aria-label="Post new content"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path>
-            <circle cx="12" cy="13" r="4"></circle>
-          </svg>
-          <span>POST</span>
-        </button>
+
       </div>
     </div>
   );
